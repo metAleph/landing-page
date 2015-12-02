ccb7ff481a8014c4982cf12b04866e881410f95b
Daniel Llatas Spiers <danllatas@gmail.com>
2015-12-02 21:02:12 +0100

    First release

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ var Handlebars = require("handlebars");
 
 $(function () {
     "use strict";
-	var longContent = "According to Borges: '... the Aleph is a point in space that contains all other points. Anyone who gazes into it can see everything in the universe from every angle simultaneously, without distortion, overlapping or confusion.' ... and meta goes for abstraction: an Aleph explaining itself. This is a data startup!!!";
-	var authors = "Cristhian Seminario {Economics, 1988}, Daniel Llatas {Software, 1989}";
-
-    var templates = [
+	var longContent = "According to Borges: '... the Aleph is a point in space that contains all other points. Anyone who gazes into it can see everything in the universe from every angle simultaneously, without distortion, overlapping or confusion ...'";
+	var templates = [
         ["#page-header-home",".hb-page-header-home",{title: 'metAleph', content: "Touching what you can't see!"}],
-        ["#page-header-about",".hb-page-header-about",{title: 'About', content: longContent}],
+        ["#page-header-about",".hb-page-header-about",{title: 'About', content: longContent, content3: "and meta goes for abstraction: an Aleph explaining itself." , content2: "This is a data - driven startup!!!"}],
         ["#page-header-project",".hb-page-header-project",{title: 'Projects', content: "eese - data", link: "http://www.metaleph.com/eese-data"}],
-        ["#page-header-team",".hb-page-header-team",{title: 'Team', content: authors}],
+        ["#page-header-team",".hb-page-header-team",{title: 'Team', author1:"Cristhian Seminario: {Economics, 1988}", author2: "Daniel Llatas: {Software, 1989}"}],
         ["#footer",".hb-footer",{content: "metAleph 2015"}]
 	];
-
     function render(template) {
     	for(var i=0;i<template.length;i++){
     		var templateCompiled = Handlebars.compile($(template[i][0]).html());
@@ -22,10 +19,5 @@ $(function () {
     		$(template[i][1]).html(html);
     	}
     }
-
     render(templates);
-
 });
-
-
-
