6c37312d59812177581e53dbae79457db7f05dda
dllatas <danllatas@gmail.com>
2016-10-21 10:20:16 +0200

    Cleaning up files for new version

---
+++ src/content.js
@@ -0,0 +1,13 @@
+export const data = {
+    headerTitle: "metAleph"
+    , headerSubtitle: "Touching what you can't see!"
+    , about: "According to Borges: '... the Aleph is a point in space that contains all other points. Anyone who gazes into it can see everything in the universe from every angle simultaneously, without distortion, overlapping or confusion ...' \n and meta goes for abstraction: an Aleph explaining itself. \n This is a data - driven startup!"
+    , project: [
+        {   key: "1", title:"EESE Data", url:"http://www.metaleph.com/eese-data"}
+    ]
+    , team: [
+        {   key: "1", name: "Cristhian Seminario", field: "Economics", year: "1988"}
+        , { key: "2", name: "Daniel Llatas", field: "Software", year: "1989"}
+    ]
+    , footer: "metAleph 2016"
+}
